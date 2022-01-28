fd33cee297fdb207c628943634d9feca931c1283
Ellie Mullen <elldabelle@Ellies-MacBook-Air.local>
2022-01-28 17:35:40 -0500

    sus

--- src/main/java/frc/robot/subsystems/ClimbingArmHook.java
+++ src/main/java/frc/robot/subsystems/ClimbingArmHook.java
@@ -10,16 +10,26 @@ public class ClimbingArmHook extends SubsystemBase {
 
     //Stepper motor
     //limit switch
-
-  /** Creates a new ExampleSubsystem. */
+ 
+    //Creates an example subsystem
   public ClimbingArmHook() {}
 
+
+// This function moves the climbing hook upwards, but only when the limit switch on the top is not
+// active. This is when the hook is not extended fully.
+// It moves until the limit switch on the top is active and then stops
+// moving. This is when the hook is extended fully.
  public void erectHook(){
-
+// The function moves a motor to move a chain positively, which only raises the hook if the top limit switch isn't active. 
+// When the top limit switch becomes active, the motor and chain stop moving.
  }
-
+// This function moves the climbing hook downwards, but only when the limit switch on the bottom is not
+// active. This is when the hook is not retracted fully.
+// It moves until the limit switch on the bottom is active and then stops
+// moving. This is when the hook is retracted fully.
  public void retractHook(){
-
+  // The function moves a motor to move a chain negatively, which only lowers/*  */ the hook if the bottom limit switch isn't active. 
+ // When the top limit switch becomes active, the motor and chain stop moving.
  }
 
  
