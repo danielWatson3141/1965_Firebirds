ab662114c2d4968e65e7309d2f5a72b351315634
danielwatson <danielwatson3141@gmail.com>
2022-01-28 17:15:51 -0500

    Add example function description to example subsystem

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -19,4 +19,31 @@ public class ExampleSubsystem extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+  //Extends the robots arm to push the button in front of it.
+  //If the arm is already extended, this does nothing
+  //If the arm is retracted, it extends
+  //Once this function is called, the arm stays extended until retractArm is called
+  //The arm takes 1 second to retract
+
+  public void extendArm(){
+    //Requires: Servo motor 1 and 2, and proximity sensor 1
+
+    //if proximity sensor detects button && arm is retracted
+    
+      //move servo motor 1 to extended position
+
+      //move servo motor 2 to extended position
+
+    //else
+      //do nothing 
+  }
+
+  public void retratArm(){
+    //Requires: Servo motor 1 and 2
+    
+    //move servo motor 1 to retracted position
+
+    //move servo motor 2 to retracted position
+  }
 }
