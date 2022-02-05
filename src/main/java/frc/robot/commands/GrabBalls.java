399572839eac691d00f328b1d0107fb5a679ed36
Owen Weitzler <owenweitzler@Owens-MacBook-Pro.local>
2022-02-05 16:44:19 -0500

    add stuff in grabballs

--- src/main/java/frc/robot/commands/GrabBalls.java
+++ src/main/java/frc/robot/commands/GrabBalls.java
@@ -1,16 +1,18 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-//amonguys
+//RIP Kurt Cobain
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Cannon;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class GrabBalls extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final Cannon cannon;
+  private final Intake intake;
 
  //eject mode: peg 1 drops
     //storage mode: peg 1-3 raises
@@ -25,17 +27,23 @@ public class GrabBalls extends CommandBase {
    *
    * @param subsystem The subsystem used by this command.
    */
-  public GrabBalls(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public GrabBalls(Cannon c, Intake i) {
+    cannon = c;
+    intake = i;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(cannon, intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
+  aButton.whenPressed(
+    new RunCommand(() -> cannon.toggleBelt(true), cannon));
+  aButton.whenReleased(
+    new RunCommand(() -> cannon.toggleBelt(false), cannon));
+  
+    // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {}
 
