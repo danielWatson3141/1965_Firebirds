90696572272e8a43bae2158e1d14d728c7dab66c
danielwatson <danielwatson3141@gmail.com>
2022-02-05 14:16:51 -0500

    Add erect hook button binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,45 +7,71 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ClimbingArmHook;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SixWheelDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SixWheelDrivetrain drivetrain = new SixWheelDrivetrain();
+  private ClimbingArmHook arm = new ClimbingArmHook();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   private XboxController myController = new XboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private JoystickButton aButton = new JoystickButton(myController, XboxController.Button.kA.value);
+  private JoystickButton bButton = new JoystickButton(myController, XboxController.Button.kB.value);
+  private JoystickButton xButton = new JoystickButton(myController, XboxController.Button.kX.value);
+  private JoystickButton yButton = new JoystickButton(myController, XboxController.Button.kY.value);
+
+  private JoystickButton lbButton = new JoystickButton(myController, XboxController.Button.kLeftBumper.value);
+  private JoystickButton rbButton = new JoystickButton(myController, XboxController.Button.kRightBumper.value);
+
+  private JoystickButton backButton = new JoystickButton(myController, XboxController.Button.kBack.value);
+  private JoystickButton startButton = new JoystickButton(myController, XboxController.Button.kStart.value);
+
+  private JoystickButton leftStickButton = new JoystickButton(myController, XboxController.Button.kLeftStick.value);
+  private JoystickButton rightStickButton = new JoystickButton(myController, XboxController.Button.kRightStick.value);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
     drivetrain.setDefaultCommand(
         // A split-stick arcade command, with forward/backward controlled by the left
         // hand, and turning controlled by the right.
-    new RunCommand(
-        () ->
-            drivetrain.drive(),
-        drivetrain));
+        new RunCommand(
+            () -> drivetrain.drive(),
+            drivetrain));
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    rbButton.whenPressed(
+        new RunCommand(() -> arm.erectHook(), arm));
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -54,6 +80,8 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return new RunCommand(
+        () -> drivetrain.drive(),
+        drivetrain);
   }
 }
