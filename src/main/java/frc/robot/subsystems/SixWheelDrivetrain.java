6028c55496169dd0adff7a2dc1e89aee0be50c42
ALEX BOYLE <alexander.boyle@saintjosephprep.org>
2023-02-25 11:49:03 -0500

    add rev blinkin

--- src/main/java/frc/robot/subsystems/SixWheelDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SixWheelDrivetrain.java
@@ -35,7 +35,7 @@ public class SixWheelDrivetrain extends SubsystemBase {
 
   public static boolean driveOverride = false;
 
-  WPI_TalonSRX m_blinkin = new WPI_TalonSRX(8);
+  WPI_TalonSRX m_blinkin = new WPI_TalonSRX(4);
 
   /** Creates a new SixWheelDrivetrain. */
   public SixWheelDrivetrain(XboxController controller) {
