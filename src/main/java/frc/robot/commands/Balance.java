89ee65ff242790d53e1534ca10fd85a8fcc3d2a7
danielwatson <danielwatson3141@gmail.com>
2023-03-11 13:29:49 -0500

    format balance code

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -5,19 +5,19 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Balance extends CommandBase{
+public class Balance extends CommandBase {
 
-    SixWheelDrivetrain sixWheelDrivetrain;
-    // Called when the command is initially scheduled.
-    
-    private final Gyro m_gyro = new ADXRS450_Gyro();
+  SixWheelDrivetrain sixWheelDrivetrain;
+  // Called when the command is initially scheduled.
 
-    public double getHeading() {
+  private final Gyro m_gyro = new ADXRS450_Gyro();
 
-      return m_gyro.getRotation2d().getDegrees();
-  
-    }
-  
+  public double getHeading() {
+
+    return m_gyro.getRotation2d().getDegrees();
+
+  }
+
   public Balance(SixWheelDrivetrain my_SixWheelDrivetrain) {
     sixWheelDrivetrain = my_SixWheelDrivetrain;
   }
@@ -26,27 +26,27 @@ public class Balance extends CommandBase{
 
   private SixWheelDrivetrain drivetrain;
 
-@Override
+  @Override
   public void initialize() {
 
-  } 
+  }
 
   final double DRIVE_SPEED_SLOW = .1;
   public final double GYRO_NEAR_ZERO_VALUE = 5;
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
     if (Math.abs(gyroValue) <= GYRO_NEAR_ZERO_VALUE) {
-      drivetrain.goAtSpeed(0); 
-    }
-    else if (gyroValue < 0) {
+      drivetrain.goAtSpeed(0);
+    } else if (gyroValue < 0) {
       drivetrain.goAtSpeed(DRIVE_SPEED_SLOW);
+    } else {
+      drivetrain.goAtSpeed(-DRIVE_SPEED_SLOW);
     }
-    else {
-      drivetrain.goAtSpeed(-DRIVE_SPEED_SLOW); }
-    
-    }
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
