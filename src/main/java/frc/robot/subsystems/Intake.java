aee340198a294b003b3187c8c17a0a8467fa210c
Ellie <ellie.mullen@saintjosephprep.org>
2024-01-20 15:14:07 -0500

    fixed intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-    public class Intake extends SubsystemBase
+    public class Intake extends SubsystemBase {
      public void turnIntakeOn(){
 
     }
