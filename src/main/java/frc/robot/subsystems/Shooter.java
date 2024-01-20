cb3fb989dfe265a389764a63b320f62b2206d34d
AGreen-Crab <camaroni.katz@gmail.com>
2024-01-20 17:03:19 -0500

    wow we did a lot to the shooter and logging

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,44 +13,76 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Logging;
 
 
-public class Shooter extends SubsystemBase{
+public class Shooter extends SubsystemBase {
+
+    final long shooterTimer = 2000;
+    final double shooterSpeed = 0.5;
+
+    final int shooterState__WaitSpinup = 1;
+    final int shooterState__WaitCan    = 2;
+    final int shooterState__Done       = 3;
+    int shooterState                   = 0;
+
+    private long timeWhenPressed;
     
-    WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(9);
-    WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(8);
-    WPI_TalonSRX canMotor = new WPI_TalonSRX(7);
-
-    long elapsedTime = 0;
-    long timeWhenPressed = 0;
-
-    double shooterTimer = 2000;
-
-
-    public boolean shooterMotorDelay() {
-        if (elapsedTime >= shooterTimer) {
-            return true;
-        } else {
-            return false;
-        }
+    public Shooter() {
+        Logging.log("Shooter:Shooter", "Constuctor");
+    
+        WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(9);
+        WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(8);
+        WPI_TalonSRX canMotor = new WPI_TalonSRX(7);
+    
+        shooterState = shooterState__WaitSpinup;
+        this.shooterInit();
     }
 
-
-    public void shooterAccel() {
+    // What does this do?
+    // This determines if the requested time has elapsed
+    public boolean CheckTimer(long timerValue) {
+        long elapsedTime = System.currentTimeMillis() - timeWhenPressed;
+        return(elapsedTime >= shooterTimer);
+    }
+    
+    public void StartTimer() { 
+        Logging.log("Shooter:StartTimer", "started timer");
         timeWhenPressed = System.currentTimeMillis();
+     }
+    
+     public void shooterInit() {
+        StartTimer();
+        shooterMotor1.set(shooterSpeed);
+        shooterMotor2.set(shooterSpeed);
+    }
 
-        shooterMotor1.set(.5);
-        shooterMotor2.set(.5);
+    public void startCan() {
+        StartTimer();
+        canMotor.set(shooterSpeed);
+    }
 
-        if (shooterMotorDelay()) {
-            shooterMotor1.set(0);
-            shooterMotor2.set(0);
-        }
-        }
-
+    public void shooterStop() {
+        shooterMotor1.set(0);
+        shooterMotor2.set(0);
+        canMotor.set(0);
+    }
 
     public void periodic() {
-        elapsedTime = System.currentTimeMillis() - timeWhenPressed;
-
-        shooterMotorDelay();
+        switch (shooterState) {
+            case shooterState__WaitSpinup:
+                if (this.CheckTimer(shooterTimer)) {
+                    Logging.log("Shooter:periodic", "Initial timer expires");
+                    this.startCan();
+                    shooterState = shooterState__WaitCan;
+                }
+                break;
+            case shooterState__WaitCan:
+                if (this.CheckTimer(shooterTimer)) { 
+                    Logging.log("Shooter:periodic", "Final timer expires - all done");
+                    this.shooterStop();
+                    shooterState = shooterState__Done;
+                }
+                break;
+            default:
+                break;
+        }
     }
-
 }
