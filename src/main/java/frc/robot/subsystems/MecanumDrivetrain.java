efc4203c19a816f63eabb3a6f591427d891bd144
Ellie <ellie.mullen@saintjosephprep.org>
2024-01-27 14:35:50 -0500

    bound intake&added field oriented drive+gyro reset

--- src/main/java/frc/robot/subsystems/MecanumDrivetrain.java
+++ src/main/java/frc/robot/subsystems/MecanumDrivetrain.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
@@ -10,62 +11,68 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-
+import frc.robot.Logging;
 
 public class MecanumDrivetrain extends SubsystemBase {
 
-    public final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+  public final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
 
-    //public double gyroAngle = m_gyro.getAngle();
+  // public double gyroAngle = m_gyro.getAngle();
 
-    private SlewRateLimiter rotationLimiter;
-    private SlewRateLimiter throttleLimiterX;
-    private SlewRateLimiter throttleLimiterY;
+  private SlewRateLimiter rotationLimiter;
+  private SlewRateLimiter throttleLimiterX;
+  private SlewRateLimiter throttleLimiterY;
 
-    // private double locationX = 0.2794;
-    // private double locationY = 0.3048;
+  // private double locationX = 0.2794;
+  // private double locationY = 0.3048;
 
-    Joystick m_stick;
+  Joystick m_stick;
 
-    Spark m_frontLeft;
-    Spark m_rearLeft;
-    Spark m_frontRight;
-    Spark m_rearRight;
+  Rotation2d gyroAngle;
 
-    private double rotationRate = 0.3;
-    private double throttleRate = 0.2;
+  Spark m_frontLeft;
+  Spark m_rearLeft;
+  Spark m_frontRight;
+  Spark m_rearRight;
 
+  private double rotationRate = 0.3;
+  private double throttleRate = 0.2;
 
-    MecanumDrive m_robotDrive;
+  MecanumDrive m_robotDrive;
 
+  /*
+   * ChassisSpeeds chassisSpeed = new ChassisSpeeds(m_stick.getY(),
+   * m_stick.getX(), 0);
+   * 
+   * Translation2d m_frontLeftLocation = new Translation2d(locationX, locationY);
+   * Translation2d m_frontRightLocation = new Translation2d(locationX,
+   * -locationY);
+   * Translation2d m_rearLeftLocation = new Translation2d(-locationX, locationY);
+   * Translation2d m_rearRightLocation = new Translation2d(-locationX,
+   * -locationY);
+   * 
+   * MecanumDriveKinematics m_kinematics = new
+   * MecanumDriveKinematics(m_frontLeftLocation, m_frontRightLocation,
+   * m_rearLeftLocation, m_rearRightLocation);
+   * 
+   * 
+   * ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
+   * MecanumDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(speeds);
+   * 
+   * double frontLeft = wheelSpeeds.frontLeftMetersPerSecond;
+   * double frontRight = wheelSpeeds.frontRightMetersPerSecond;
+   * double backLeft = wheelSpeeds.rearLeftMetersPerSecond;
+   * double backRight = wheelSpeeds.rearRightMetersPerSecond;
+   */
 
-  /*  ChassisSpeeds chassisSpeed = new ChassisSpeeds(m_stick.getY(), m_stick.getX(), 0);
-
-    Translation2d m_frontLeftLocation = new Translation2d(locationX, locationY);
-    Translation2d m_frontRightLocation = new Translation2d(locationX, -locationY);
-    Translation2d m_rearLeftLocation = new Translation2d(-locationX, locationY);
-    Translation2d m_rearRightLocation = new Translation2d(-locationX, -locationY);
-
-    MecanumDriveKinematics m_kinematics = new MecanumDriveKinematics(m_frontLeftLocation, m_frontRightLocation, m_rearLeftLocation, m_rearRightLocation);
-
-
-    ChassisSpeeds speeds = new ChassisSpeeds(1.0, 3.0, 1.5);
-    MecanumDriveWheelSpeeds wheelSpeeds = m_kinematics.toWheelSpeeds(speeds);
-
-    double frontLeft = wheelSpeeds.frontLeftMetersPerSecond;
-    double frontRight = wheelSpeeds.frontRightMetersPerSecond;
-    double backLeft = wheelSpeeds.rearLeftMetersPerSecond;
-    double backRight = wheelSpeeds.rearRightMetersPerSecond;  */
-
-    
-    public MecanumDrivetrain(Joystick input_stick){
+  public MecanumDrivetrain(Joystick input_stick){
       m_frontLeft = new Spark(4);
       m_rearLeft = new Spark(6);
       m_frontRight = new Spark(3);
@@ -81,19 +88,30 @@ public class MecanumDrivetrain extends SubsystemBase {
       rotationLimiter = new SlewRateLimiter(rotationRate);
       throttleLimiterX = new SlewRateLimiter(throttleRate);
       throttleLimiterY = new SlewRateLimiter(throttleRate);
+
+      ADXRS450_Gyro m_gyro = 
     }
 
-  //multipliers for values
+  // multipliers for values
   private double speedCap = .6;
-  private double rotateCap = .6; 
+  private double rotateCap = .6;
+
+  public Rotation2d gyroAngle() {
+    return m_gyro.getRotation2d();
+  }
+
+  public void gyroReset() {
+    m_gyro.reset();
+    Logging.log(getSubsystem(), "reset gyroscope");
+  }
 
   public void drive() {
     m_robotDrive.driveCartesian(
-      throttleLimiterX.calculate(m_stick.getX() * speedCap),
-      throttleLimiterY.calculate(m_stick.getY() * speedCap),
-      rotationLimiter.calculate(m_stick.getZ() * rotateCap)
-      );
-   
+        throttleLimiterX.calculate(m_stick.getX() * speedCap),
+        throttleLimiterY.calculate(m_stick.getY() * speedCap),
+        rotationLimiter.calculate(m_stick.getZ() * rotateCap),
+        m_gyro.getRotation2d());
+
     SmartDashboard.putNumber("stickX", m_stick.getX());
     SmartDashboard.putNumber("stickY", m_stick.getY());
     SmartDashboard.putNumber("stickZ", m_stick.getZ());
