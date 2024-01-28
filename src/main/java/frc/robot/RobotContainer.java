3e4fa6347bec406ad3003f8632f1eb43ec9a08b5
AGreen-Crab <camaroni.katz@gmail.com>
2024-01-27 20:03:56 -0500

    so uh... forgot to import some things so here, also i fixed some merge conflics for me

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,11 +11,13 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Lifter;
 import frc.robot.subsystems.MecanumDrivetrain;
 import frc.robot.subsystems.Shooter;
 
