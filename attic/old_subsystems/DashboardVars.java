35be2f7d49a648966673f8d1bd3c627663ba4f30
1965 <1965@stjosephprep.edu>
2024-02-03 16:47:02 -0500

    changed some dashboard values

--- attic/old_subsystems/DashboardVars.java
+++ attic/old_subsystems/DashboardVars.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//********FOR DOCUMENTATION PURPOSES********
 public class DashboardVars extends SubsystemBase {
 
     double exampleVar;
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,99 +9,98 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Logging;
 
-
 public class Shooter extends SubsystemBase {
 
-    final long SHOOTER_TIMER_SEC = 2;
-    public double shooterTimerTest;
     private double shooterSpeed = .5;
 
     private WPI_TalonSRX shooterMotor1;
     private WPI_TalonSRX shooterMotor2;
     private WPI_TalonSRX canMotor;
 
-    public double shooterSpeedTest;
+    private boolean iron_man = true;
+    private double shooterTimerTest = 2;
+    private double shooterSpeedTest = .22;
 
     public Shooter() {
         Logging.log("Shooter:Shooter", "Constuctor");
-    
-        //sets motors to port number
+
+        // sets motors to port number
         shooterMotor1 = new WPI_TalonSRX(3);
         shooterMotor2 = new WPI_TalonSRX(4);
         canMotor = new WPI_TalonSRX(7);
- 
-        SmartDashboard.putNumber("Shooter Speed Slider", .22);
-        SmartDashboard.putNumber("Shooter Timer Slider", 2);
+
+        if (iron_man) {
+            SmartDashboard.putNumber("Shooter Speed Slider", shooterSpeedTest);
+            SmartDashboard.putNumber("Shooter Timer Slider", shooterTimerTest);
+        }
     }
-    
-    //sets shooting motor's voltage to the variable
-     public void shooterSpinup() {
+
+    // sets shooting motor's voltage to the variable
+    public void shooterSpinup() {
         Logging.log(getSubsystem(), "Shooter Speed is " + shooterSpeed);
         shooterMotor1.set(shooterSpeed);
         shooterMotor2.set(shooterSpeed);
     }
-        
-    //sets the can motor's votage to the variable
+
+    // sets the can motor's votage to the variable
     public void startCan() {
         canMotor.set(shooterSpeed);
     }
 
-    //stops all motors
+    // stops all motors
     public void shooterStop() {
         shooterMotor1.set(0);
         shooterMotor2.set(0);
         canMotor.set(0);
     }
 
-    public Command getShootCommand(){
-        Command r_command = 
-        Commands.sequence(
-                //Starts up the shooter motors
+    public Command getShootCommand() {
+        Command r_command = Commands.sequence(
+                // Starts up the shooter motors
                 new InstantCommand(() -> shooterSpinup()),
-                //waits for variable miliseconds
-                Commands.waitSeconds(SHOOTER_TIMER_SEC),
-                //Starts can motor
+                // waits for variable miliseconds
+                Commands.waitSeconds(shooterTimerTest),
+                // Starts can motor
                 new InstantCommand(() -> startCan()),
-                //wait for variable miliseconds
-                Commands.waitSeconds(SHOOTER_TIMER_SEC),
-                //stop all motors
-                new InstantCommand(() -> shooterStop())
-            );
+                // wait for variable miliseconds
+                Commands.waitSeconds(shooterTimerTest),
+                // stop all motors
+                new InstantCommand(() -> shooterStop()));
         r_command.addRequirements(this);
         return r_command;
     }
 
-    public Command testShootRunCommand(){
-        Command r_command = 
-            new InstantCommand(() -> shooterSpinup());
+    public Command testShootRunCommand() {
+        Command r_command = new InstantCommand(() -> shooterSpinup());
         r_command.addRequirements(this);
         return r_command;
     }
 
-      public Command testShootStopCommand(){
-        Command r_command = 
-            new InstantCommand(() -> shooterStop());
+    public Command testShootStopCommand() {
+        Command r_command = new InstantCommand(() -> shooterStop());
         r_command.addRequirements(this);
         return r_command;
     }
+
     /*
-        VERY IMPORTANT (\0_0)\ 
-        This function works by calling the function and putting the desired speed into the requirements.
-        It will not work if you do "motorSet()"
-        It HAS to have a value like "motorSet(.2)"
-    */
+     * VERY IMPORTANT (\0_0)\
+     * This function works by calling the function and putting the desired speed
+     * into the requirements.
+     * It will not work if you do "motorSet()"
+     * It HAS to have a value like "motorSet(.2)"
+     */
     public void shooterMotorSet(double setSpeed) {
         shooterSpeed = setSpeed;
         double speedPercentage = shooterSpeed * 100;
 
-        String speed = "speed is at " + speedPercentage + " percent";
-        
         SmartDashboard.putNumber("Shooter %", speedPercentage);
     }
 
     @Override
     public void periodic() {
-        shooterSpeedTest = SmartDashboard.getNumber("Shooter Speed Slider", 0);
-        shooterTimerTest = SmartDashboard.getNumber("Shooter Timer Slider", 2);
+        if (iron_man) {
+            shooterSpeedTest = SmartDashboard.getNumber("Shooter Speed Slider", 0);
+            shooterTimerTest = SmartDashboard.getNumber("Shooter Timer Slider", 0);
+        }
     }
 }
