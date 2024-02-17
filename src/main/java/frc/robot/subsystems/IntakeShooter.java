c5fe0fe05abae7fe91810e54357b010cd014ecb1
1965 <1965@stjosephprep.edu>
2024-02-17 16:46:46 -0500

    intake command sequence prototype (nonfunctional)

--- src/main/java/frc/robot/subsystems/IntakeShooter.java
+++ src/main/java/frc/robot/subsystems/IntakeShooter.java
@@ -106,7 +106,7 @@ public class IntakeShooter extends SubsystemBase {
 
     public Command getIntakeCommand() {
         Command r_command = (
-           (new InstantCommand(() -> setIntakeMotors(INTAKE_SPEED)).until(() -> switch2State())).withTimeout(INTAKE_TIMEOUT_SECONDS)
+           (new InstantCommand(() -> setIntakeMotors(INTAKE_SPEED)).until(() -> switch2State())).withTimeout(INTAKE_TIMEOUT_SECONDS).andThen(new InstantCommand(() -> setIntakeMotors(0)))
         );
 
         r_command.addRequirements(this);
