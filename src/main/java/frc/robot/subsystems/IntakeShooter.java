123174d6c88071fd246917c0446d87cec4009006
Ellie <ellie.mullen@saintjosephprep.org>
2024-02-24 09:24:19 -0500

    removed parenthesis from decorators

--- src/main/java/frc/robot/subsystems/IntakeShooter.java
+++ src/main/java/frc/robot/subsystems/IntakeShooter.java
@@ -104,9 +104,10 @@ public class IntakeShooter extends SubsystemBase {
         return r_command;
     }
 
+
     public Command getIntakeCommand() {
         Command r_command = (
-           (new InstantCommand(() -> setIntakeMotors(INTAKE_SPEED)).until(() -> switch2State())).withTimeout(INTAKE_TIMEOUT_SECONDS)
+           new InstantCommand(() -> setIntakeMotors(INTAKE_SPEED)).until(() -> switch2State()).withTimeout(INTAKE_TIMEOUT_SECONDS)
            .andThen(new InstantCommand(() -> setIntakeMotors(0)))
         );
 
