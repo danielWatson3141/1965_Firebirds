9b401e69fee302ccb908a54e646de9cfe45542ee
Ellie <ellie.mullen@saintjosephprep.org>
2024-03-15 19:56:59 -0400

    pathplanner library + file

---
+++ src/main/java/frc/robot/subsystems/PathPlanner.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Logging;
+
+public class PathPlanner extends SubsystemBase{
+
+    public PathPlanner(MecanumDrivetrain m_drivetrain) {
+
+    }
+
+}
+
